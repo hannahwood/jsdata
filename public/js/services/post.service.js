3b26b5166f84bb7aff383f2778bdb966b8ec676a
Hannah <hannahkowood@gmail.com>
2016-04-04 18:35:31 -0400

    tis just the beginning...

--- public/js/services/post.service.js
+++ public/js/services/post.service.js
@@ -1,19 +1,38 @@
 'use strict';
 
-app.factory('Post', function(DS) {
+app.factory('Post', function(DS, $state) {
+	console.log('post factory is being run');
+    var Post = DS.defineResource({
+        name: 'posts',
+        relations: {
+            belongsTo: {
+                users: {
+                    localKey: 'author',
+                    localField: '_author'
+                }
+            }
+        },
+        methods: {
+        	go: function() {
+        		$state.go('post', {
+        			postId: this._id,
+        			authorId: this.author
+        		});
+        	}
+        }
+    });
 
-	/*
+    return Post;
+    /*
 
-		TODOS: 
-		1) create a js-data POST resource that establishes a relationship 
-		between posts and users
+    	TODOS: 
+    	1) create a js-data POST resource that establishes a relationship 
+    	between posts and users
 
-		2) add a methods object to the resource
-				a) add a `go` method that takes each post to the individual post page 
-				(HINT: see post.js)
+    	2) add a methods object to the resource
+    			a) add a `go` method that takes each post to the individual post page 
+    			(HINT: see post.js)
 
-	*/
+    */
 
-});
-
-
+}).run(function (Post) {});
--- public/js/services/user.service.js
+++ public/js/services/user.service.js
@@ -1,10 +1,8 @@
-'use  strict';
-
+'use strict';
 app.factory('User', function(DS) {
-	
-  /*
-    create a User jsdata resource 
-  */
+  return DS.defineResource({
+  	name: 'users'
+  });
 
-})
+}).run(function (User) {});
 
